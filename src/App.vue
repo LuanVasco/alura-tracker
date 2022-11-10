50d5c838cf7ec52917928fc3a2c8f6e0e6d2f2d8
Luan Vasco <devluangodoy@gmail.com>
2022-11-10 08:16:55 -0300

    feat: instalando vue router 4 e configurando view e rotas

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <FormularioPlay @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <TarefaItem 
-          v-for="(tarefa, index) in tarefas"
-          :key="index"
-          :tarefa="tarefa"
-        />
-        <BoxTask v-if="listaEstaVazia">
-          Você não está muito produtivo hoje :(
-        </BoxTask>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -24,34 +14,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from '@/components/BarraLateral.vue';
-import FormularioPlay from '@/components/FormularioPlay.vue';
-import TarefaItem from '@/components/TarefaItem.vue';
-import BoxTask from '@/components/BoxTask.vue';
-import ITarefa from '@/interfaces/ITarefa';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    FormularioPlay,
-    TarefaItem,
-    BoxTask
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false,
     }
   },
-  computed: {
-    listaEstaVazia() : boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
     trocarTema(modoEscuroAtivo : boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
@@ -60,9 +34,6 @@ export default defineComponent({
 </script>
 
 <style>
-.lista {
-  padding: 1.25rem
-}
 main {
   --bg-primario: #fff;
   --texto-primario: #000;
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,52 @@
+<template>
+    <FormularioPlay @aoSalvarTarefa="salvarTarefa"/>
+    <div class="lista">
+        <TarefaItem 
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+        :tarefa="tarefa"
+        />
+        <BoxTask v-if="listaEstaVazia">
+            Você não está muito produtivo hoje :(
+        </BoxTask>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import FormularioPlay from '@/components/FormularioPlay.vue';
+  import TarefaItem from '@/components/TarefaItem.vue';
+  import BoxTask from '@/components/BoxTask.vue';
+  import ITarefa from '@/interfaces/ITarefa';
+  
+  export default defineComponent({
+    name: 'App',
+    components: {
+      FormularioPlay,
+      TarefaItem,
+      BoxTask
+    },
+    data() {
+      return {
+        tarefas: [] as ITarefa[],
+      }
+    },
+    computed: {
+      listaEstaVazia() : boolean {
+        return this.tarefas.length === 0
+      }
+    },
+    methods: {
+      salvarTarefa(tarefa: ITarefa) {
+        this.tarefas.push(tarefa)
+      },
+    }
+  });
+  </script>
+
+<style>
+.lista {
+  padding: 1.25rem
+}
+</style>
+  
