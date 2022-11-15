cea4ec68c59b2e1d8689bc53a57b025c7b731a8d
Luan Vasco <devluangodoy@gmail.com>
2022-11-15 16:21:58 -0300

    feat: criando mixin para desprender das regras do store toda vez que for enviar uma notificacao

--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -22,8 +22,9 @@
 <script lang="ts">
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 import { useStore } from "@/store";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes";
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from "@/store/tipo-mutacoes";
 import { defineComponent } from "vue";
+import { notificacaoMixin } from "@/mixins/notificar"
 
 export default defineComponent ({
     name: 'FormularioProjetos',
@@ -32,6 +33,7 @@ export default defineComponent ({
             type: String
         }
     },
+    mixins: [notificacaoMixin],
     mounted() {
         if(this.id) {
             const projeto = this.store.state.projetos.find(projeto => projeto.id == this.id)
@@ -54,13 +56,9 @@ export default defineComponent ({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '';
-            this.store.commit(NOTIFICAR, {
-                titulo: 'Novo projeto foi salvo',
-                texto: 'Prontinho ;) seu projeto já está disponível.',
-                tipo: TipoNotificacao.SUCESSO
-            })
+            this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
             this.$router.push('/projetos')
-        }
+        },
     },
     setup() {
         const store = useStore()
