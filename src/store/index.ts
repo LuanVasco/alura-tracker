e5add075102328edd727b3a9776d31c9bfd3e413
Luan Vasco <devluangodoy@gmail.com>
2022-11-16 07:40:52 -0300

    feat: criando baseUrl para requisicoes e get atravez do actions commit na stateProjetos

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as VuexUseStore } from "vuex";
 import { InjectionKey } from 'vue';
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-
+import { OBTER_PROJETOS } from "./tipo-acoes";
+import http from "@/http";
 interface Estado {
     projetos: IProjeto[],
     notificacoes: INotificacao[]
@@ -31,6 +32,9 @@ export const store = createStore<Estado>({
         [EXCLUIR_PROJETO](state, id: string) {
             state.projetos = state.projetos.filter(projeto => projeto.id != id)
         },
+        [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
+            state.projetos = projetos
+        },
         [NOTIFICAR](state, novaNotificacao: INotificacao) {
             novaNotificacao.id = new Date().getTime()
             state.notificacoes.push(novaNotificacao)
@@ -40,6 +44,11 @@ export const store = createStore<Estado>({
                     notificacao => notificacao.id != novaNotificacao.id
                 )
             }, 3000)
+        }
+    },
+    actions: {
+        [OBTER_PROJETOS] ({ commit }) {
+            http.get('projetos').then(res => commit(DEFINIR_PROJETOS, res.data))
         }
     }
 })
